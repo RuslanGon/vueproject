afbe5cb70e0ef3daff991e03fe56495e18f64413
RuslanGon <ruslan441006@gmail.com>
2024-10-27 11:38:09 +0200

    wer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 
 <template>
-
-  <MyHello />
-
+<h1>hello</h1>
+<h3>mmam</h3>
 </template>
 
 <script setup>
-import MyHello from './components/MyHello.vue'
+// import MyHello from './components/MyHello.vue'
 
 </script>
 
