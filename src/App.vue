130f287a3ee1c4a1e8f3012c727811a388c62795
RuslanGon <ruslan441006@gmail.com>
2024-10-28 16:56:54 +0200

    wer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
 <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-<header class="flex justify-between border-b border-slate-300 px-8">
+<header class="flex justify-between border-b border-slate-300 px-10 py-8">
   <div class="flex items-center gap-4">
     <img class="w-10" src="/logo.png" alt="logo">
     <div>
@@ -10,10 +10,10 @@
     </div>
   </div>
 
-  <ul class="flex items-center gap-10">
-    <li class="flex items-center gap-3"><img src="/cart.svg" alt="cart"><b>1205 руб</b></li>
-    <li class="flex items-center gap-3"><img src="/heart.svg" alt="heart"><b>Закладки</b></li>
-    <li class="flex items-center gap-3"><img src="/profile.svg" alt="cart"><b>Профиль</b></li>
+  <ul class="flex items-center gap-10 text-slate-500">
+    <li class="flex items-center gap-3"><img src="/cart.svg" alt="cart"><span>1205 руб</span></li>
+    <li class="flex items-center gap-3"><img src="/heart.svg" alt="heart"><span>Закладки</span></li>
+    <li class="flex items-center gap-3"><img src="/profile.svg" alt="cart"><span>Профиль</span></li>
   </ul>
 </header>
 </div>
