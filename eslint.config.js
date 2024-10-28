d29339e74a419b1d03c116a92c485426a329d946
RuslanGon <ruslan441006@gmail.com>
2024-10-28 17:20:08 +0200

    wer

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,15 @@ import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+export default {
+  files: ['**/*.{js,mjs,jsx,vue}'],
+  ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
   },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-    rules: {
-      'vue/multi-word-component-names': 'off',
-    },
-  },
-  
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  skipFormatting,
-]
+  extends: [
+    js.configs.recommended,
+    ...pluginVue.configs['flat/essential'],
+    skipFormatting,
+  ],
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: '#FFD700', 
+      },
+    },
   },
   plugins: [],
 }
