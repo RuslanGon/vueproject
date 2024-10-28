5d02861edb04f9b90a3fcc2d28ab83258b9e4f37
RuslanGon <ruslan441006@gmail.com>
2024-10-28 16:51:13 +0200

    wer

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@
   <div class="flex items-center gap-4">
     <img class="w-10" src="/logo.png" alt="logo">
     <div>
-      <h2>REACT SNEAKERS</h2>
-      <p>Магазин лучших кроссовок</p>
+      <h2 class="text-xl font-bold uppercase">VUE SNEAKERS</h2>
+      <p class="text-slate-500">Магазин лучших кроссовок</p>
     </div>
   </div>
 
-  <ul>
-    <li><img src="/cart.svg" alt="cart"><b>1205 руб</b></li>
-    <li><img src="/heart.svg" alt="heart"><b>Закладки</b></li>
-    <li><img src="/profile.svg" alt="cart"><b>Профиль</b></li>
+  <ul class="flex items-center gap-10">
+    <li class="flex items-center gap-3"><img src="/cart.svg" alt="cart"><b>1205 руб</b></li>
+    <li class="flex items-center gap-3"><img src="/heart.svg" alt="heart"><b>Закладки</b></li>
+    <li class="flex items-center gap-3"><img src="/profile.svg" alt="cart"><b>Профиль</b></li>
   </ul>
 </header>
 </div>
