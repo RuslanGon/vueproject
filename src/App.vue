a894f01e8903b292d78c4b982ad6784e7950987b
RuslanGon <ruslan441006@gmail.com>
2024-10-28 17:00:28 +0200

    wer

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 
 <template>
-<div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-<header class="flex justify-between border-b border-slate-300 px-10 py-8">
-  <div class="flex items-center gap-4">
-    <img class="w-10" src="/logo.png" alt="logo">
-    <div>
-      <h2 class="text-xl font-bold uppercase">VUE SNEAKERS</h2>
-      <p class="text-slate-500">Магазин лучших кроссовок</p>
-    </div>
-  </div>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+      <div class="flex items-center gap-4">
+        <img class="w-10" src="/logo.png" alt="logo">
+        <div>
+          <h2 class="text-xl font-bold uppercase">VUE SNEAKERS</h2>
+          <p class="text-slate-500">Магазин лучших кроссовок</p>
+        </div>
+      </div>
 
-  <ul class="flex items-center gap-10 text-slate-500">
-    <li class="flex items-center gap-3"><img src="/cart.svg" alt="cart"><span>1205 руб</span></li>
-    <li class="flex items-center gap-3"><img src="/heart.svg" alt="heart"><span>Закладки</span></li>
-    <li class="flex items-center gap-3"><img src="/profile.svg" alt="cart"><span>Профиль</span></li>
-  </ul>
-</header>
-</div>
+      <ul class="flex items-center gap-10 text-slate-500">
+        <li class="flex items-center gap-3 hover:text-black cursor-pointer">
+          <img src="/cart.svg" alt="cart"><span>1205 руб</span>
+        </li>
+        <li class="flex items-center gap-3 hover:text-black cursor-pointer">
+          <img src="/heart.svg" alt="heart"><span>Закладки</span></li>
+        <li class="flex items-center gap-3 hover:text-black cursor-pointer">
+          <img src="/profile.svg" alt="cart"><span>Профиль</span></li>
+      </ul>
+    </header>
+  </div>
 </template>
 
 <script setup>
