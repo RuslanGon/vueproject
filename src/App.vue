0de9cff07b06f3c364ea910a231383e95ddf5cf6
RuslanGon <ruslan441006@gmail.com>
2024-10-29 11:58:03 +0200

    wer

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
     <Header />
     <div>
-      <div class="relative ">
-        <img class="absolute top-0 left-0" src="/like-1.svg" alt="like">
+      <div class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2">
+        <img class="absolute top-8 left-8" src="/like-1.svg" alt="like">
         <img src="/sneakers/sneakers-1.jpg" alt="sneakers">
         <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
         <div class="flex justify-between ">
