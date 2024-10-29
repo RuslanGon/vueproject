f471a0ffd6578e95ef4f75385d5cf73aa96caa40
RuslanGon <ruslan441006@gmail.com>
2024-10-29 16:24:09 +0200

    wer

---
+++ src/components/CartItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center border border-slate-300 p-4 rounded-xl gap-5">
+        <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="sneakers">
+        <div class="flex  flex-col">
+            <p>Мужские Кроссовки <br> Nike Air Max 270</p>
+            <div class="flex justify-between mt-3">
+                <b>12999 руб</b>
+                <img src="/close.svg" alt="">
+            </div>
+        </div>
+    </div>
+</template>
