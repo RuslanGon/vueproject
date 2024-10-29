1aa5965134267876bfd4e5af6996f1d4536e6fab
RuslanGon <ruslan441006@gmail.com>
2024-10-29 14:02:26 +0200

    wer

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
-        <img class="absolute top-8 left-8" src="/like-1.svg" alt="like">
+        <img @click="onClickFavorite" class="absolute top-8 left-8" :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'" alt="like">
         <img :src="imageUrl" alt="sneakers">
         <p class="mt-2">{{ tittle }}</p>
         <div class="flex justify-between mt-5">
@@ -9,7 +9,7 @@
             <span class="text-slate-400">Цена</span>
             <b>{{ price }} руб</b>
           </div>
-          <img :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="plus">
+          <img @click="onClickAdded" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="plus">
         </div>
       </div>
 </template>
@@ -20,6 +20,10 @@ defineProps({
   tittle: String,
   price: Number,
   isFavorite: Boolean,
-  isAdded: Boolean
+  isAdded: Boolean,
+  onClickAdded: Function,
+  onClickFavorite: Function
+
 })
+
 </script>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,22 @@
 <template>
-     <div class="grid grid-cols-4 gap-5">
-      <Card tittle="Мужские Кроссовки Nike Blazer Mid Suede" imageUrl="/sneakers/sneakers-1.jpg" :price="5000"/>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+  <div class="grid grid-cols-4 gap-5">
+    <Card tittle="Мужские Кроссовки Nike Blazer Mid Suede" 
+    imageUrl="/sneakers/sneakers-1.jpg" 
+    :price="5000"
+    :onClickAdded="onClickAdded"
+     />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
 </template>
 
 <script setup>
 import Card from './Card.vue';
+
+const onClickAdded = () => {
+  alert('Add')
+}
 </script>
