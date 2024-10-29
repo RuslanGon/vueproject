e658bad3aa6c54847abd57df1880943fa7221cb3
RuslanGon <ruslan441006@gmail.com>
2024-10-29 12:29:59 +0200

    wer

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
+    <h2 class="text-3xl font-bold">Все кроссовки</h2>
     <div>
-      <div class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2">
-        <img class="absolute top-8 left-8" src="/like-1.svg" alt="like">
-        <img src="/sneakers/sneakers-1.jpg" alt="sneakers">
-        <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-        <div class="flex justify-between ">
-          <div class="flex flex-col">
-            <span class="text-slate-400">Цена</span>
-            <b>12 999 руб.</b>
-          </div>
-          <img src="/plus.svg" alt="plus">
-        </div>
-      </div>
-
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   </div>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
-
+import Card from './components/Card.vue';
 
 </script>
 
