7cc8148a56d0bbc30209a7dfbe1e863685322afe
RuslanGon <ruslan441006@gmail.com>
2024-10-29 17:34:41 +0200

    wer

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-    <CardList />
+      <div class="flex justify-between">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <div class="relative">
+          <img class="absolute left-4 top-3" src="/search.svg" alt="">
+          <input class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400" placeholder="Поиск...">
+        </div>
+      </div>
+      <CardList />
     </div>
   </div>
 </template>
