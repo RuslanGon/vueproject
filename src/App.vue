683826b5a0acd0bb380366f74c81afa8f85c0859
RuslanGon <ruslan441006@gmail.com>
2024-10-29 17:43:20 +0200

    wer

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,19 @@
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
     <div class="p-10">
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+          <option>По названию</option>
+          <option>По цене (дешевая)</option>
+          <option>По цене (дорогая)</option>
+
+        </select>
         <div class="relative">
           <img class="absolute left-4 top-3" src="/search.svg" alt="">
           <input class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400" placeholder="Поиск...">
+        </div>
         </div>
       </div>
       <CardList />
