79e326c974ae5e29f26cece7f42482e45af6bed5
RuslanGon <ruslan441006@gmail.com>
2024-10-30 16:13:53 +0200

    wer

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,24 +1,32 @@
-<template>
-  <div class="grid grid-cols-4 gap-5">
-    <Card 
-    v-for="item in items" :key="item.id"
-    :title="item.title" 
-    :imageUrl="item.imageUrl" 
-    :price="item.price"
-    :onClickAdded="onClickAdded"
-    :isFavorite="false"
-     />
-  </div>
-</template>
-
 <script setup>
 import Card from './Card.vue';
 
-const onClickAdded = () => {
-alert('add')
-}
-
-defineProps({
+const props = defineProps({
   items: Array
-})
+});
+
+// Фильтрация массива, чтобы оставить только корректные объекты товаров
+const filteredItems = props.items.flatMap(item => 
+  typeof item === 'object' && 'id' in item && 'title' in item && 'price' in item && 'imageUrl' in item 
+    ? item 
+    : Object.values(item).filter(i => typeof i === 'object' && 'id' in i && 'title' in i && 'price' in i && 'imageUrl' in i)
+);
+
+const onClickAdded = () => {
+  alert('add');
+}
 </script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card 
+      v-for="item in filteredItems.slice(0, 16)" 
+      :key="item.id"
+      :title="item.title" 
+      :imageUrl="item.imageUrl" 
+      :price="item.price"
+      :onClickAdded="onClickAdded"
+      :isFavorite="false"
+    />
+  </div>
+</template>
