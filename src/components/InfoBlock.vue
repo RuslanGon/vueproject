5287af930890b73bf8bc12fa96fc08a7355218bd
RuslanGon <ruslan441006@gmail.com>
2024-11-06 17:26:15 +0200

    we

--- src/components/InfoBlock.vue
+++ src/components/InfoBlock.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-center text-center w-72 mx-auto">
+    <div class="flex flex-col items-center text-center w-72 mx-auto mt-16">
     <img height="70" width="70" :src="imageUrl" alt="title">
     <h2 class="mt-4 text-2xl font-medium">{{ title }}</h2>
     <p class="text-gray-400 mt-2">{{ desc }}</p>
