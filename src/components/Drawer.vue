c4dde14f669dcea3276cc54c27cf7e4f8632b00a
RuslanGon <ruslan441006@gmail.com>
2024-11-06 16:36:59 +0200

    wer

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -2,7 +2,7 @@
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8 flex flex-col">
     <DrawerHead />
-    <InfoBlock title="Корзина пустая" desc="Добавьте хоть одну пару крассовок, чтоб сделать заказ"/>
+    <InfoBlock title="Корзина пустая" desc="Добавьте хоть одну пару крассовок, чтоб сделать заказ" imageUrl="/package-icon.png"/>
     <div class="cart-container flex-1 overflow-y-auto">
       <CartItemList />
     </div>
