9ee1cd28a897a0aaf9c69df17207d11a5eda06ca
RuslanGon <ruslan441006@gmail.com>
2024-11-06 18:30:11 +0200

    wer

--- src/components/CartItemList.vue
+++ src/components/CartItemList.vue
@@ -1,6 +1,7 @@
 <template>
-      <div class="flex flex-col flex-1 gap-4">
-        <CartItem v-for="item in cart" :key="item.id" :title="item.title" :price="item.price" :imageUrl="item.imageUrl"  @click="() => removeFromCard(item)" />
+  <div class="flex flex-col flex-1 gap-4" v-auto-animate>
+    <CartItem v-for="item in cart" :key="item.id" :title="item.title" :price="item.price" :imageUrl="item.imageUrl"
+      @click="() => removeFromCard(item)" />
   </div>
 </template>
 
