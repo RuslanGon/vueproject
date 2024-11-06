7c376f3e502e3cd12b600f4ed107d95ab5f60c5d
RuslanGon <ruslan441006@gmail.com>
2024-11-06 17:42:09 +0200

    wer

--- src/components/InfoBlock.vue
+++ src/components/InfoBlock.vue
@@ -3,10 +3,18 @@
     <img height="70" width="70" :src="imageUrl" alt="title">
     <h2 class="mt-4 text-2xl font-medium">{{ title }}</h2>
     <p class="text-gray-400 mt-2">{{ desc }}</p>
+    <button @click="closeDrawer"
+        class="mt-8 transition bg-gray-500 w-full rounded-xl py-3 text-white disabled:bg-slate-400 hover:bg-gray-600 active:bg-gray-700 cursor-pointer">
+        Вернуться назад
+      </button>
     </div>
 </template>
 
 <script setup>
+import { inject } from 'vue';
+
+const { closeDrawer } = inject('cart');
+
 defineProps({
     imageUrl: String,
     title: String,
