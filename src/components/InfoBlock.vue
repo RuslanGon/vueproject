2b076acf6973ea51998d93fb729967680361edac
RuslanGon <ruslan441006@gmail.com>
2024-11-06 15:15:30 +0200

    wer

---
+++ src/components/InfoBlock.vue
@@ -0,0 +1,13 @@
+<template>
+    <img height="70" width="70" :src="imageUrl" alt="title">
+    <h2 class="mt-4 text-2xl font-medium">{{ title }}</h2>
+    <p class="">{{ desc }}</p>
+</template>
+
+<script setup>
+defineProps({
+    imageUrl: String,
+    title: String,
+    desc: String,
+})
+</script>
